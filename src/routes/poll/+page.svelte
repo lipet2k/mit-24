70c35db25b3f27dbb1496e8748ba44c7f5d7ab3f
Peter Li <lipet2k@Peters-MacBook-Pro.local>
2024-04-20 12:16:20 -0400

    backend

--- src/routes/poll/+page.svelte
+++ src/routes/poll/+page.svelte
@@ -1,3 +1,29 @@
-<h1>
-    Hello
-</h1>
+<script lang="ts">
+	import QrCode from '$components/QrCode/QrCode.svelte';
+	import Button from '$components/Button/Button.svelte';
+	import Input from '$components/Input/Input.svelte';
+	import Card from '$components/Card/Card.svelte';
+</script>
+
+<div class="mx-2 my-2">
+<Card>
+    <center>
+    <h2 class="mb-8">Replace floors with high quality oak?</h2>
+    <div>
+        <Button type="submit">
+        Yes
+        </Button>
+        <Button type="submit">
+        No
+        </Button>
+
+    </div>
+    </center>
+</Card>
+</div>
+
+<style lang="postcss">
+	h2 {
+		@apply text-2xl font-bold;
+	}
+</style>
