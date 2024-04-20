d871556a446918050c78142d27863e70af25bf4a
Peter Li <lipet2k@Peters-MacBook-Pro.local>
2024-04-20 19:45:24 -0400

    Add qr code

--- src/routes/buy/modal.svelte
+++ src/routes/buy/modal.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
 	import type { Property } from './types';
-	
+	import {
+		lndGetWalletBalance,
+		lndGetInfo,
+		lndListChannels,
+		lndListInvoices,
+		lndCreateInvoice,
+		lndNewAddress,
+		type GetInfoResponse
+	} from '$lib/lnd';
+	import { onMount } from 'svelte';
+	import QrCode from '$components/QrCode/QrCode.svelte';
+	import Button from '$components/Button/Button.svelte';
+	import Input from '$components/Input/Input.svelte';
+	import NetworkCapsule from '../../features/NetworkCapsule/NetworkCapsule.svelte';
+	import Card from '$components/Card/Card.svelte';
 	export let property: Property;
+
+	let invoice = '';
+	let address = '';
+	let info: GetInfoResponse;
+
+	const createInvoice = async (amount: number, memo: string) => {
+		invoice = (await lndCreateInvoice(amount, memo)).payment_request;
+	};
+
+	const getInfo = async () => {
+		info = await lndGetInfo();
+	};
+
+	onMount(async () => {
+		await getInfo().then(async () => {
+			await createInvoice(Math.ceil(Number(property.shareprice)), property.name);
+	});
+		
+	});
 
 </script>
 
@@ -11,9 +44,12 @@
 			
 			<h2>{property.name}</h2>
 			<p>{property.description}</p>
-			<p><strong>Price:</strong> ${property.totalprice}</p>
-			<p><strong>Share price:</strong> ${property.shareprice}</p>
+			<p><strong>Price:</strong> {property.totalprice} SATS</p>
+			<p><strong>Share price:</strong> {property.shareprice} SATS</p>
 			<p><strong>Percentage owned:</strong> {property.percentage}%</p>
+			{#if address || invoice}
+			<QrCode image='https://voltage.imgix.net/Team.png?fm=webp&w=160' {address} {invoice} />
+			{/if}
 		</div>
 	</div>
 
@@ -31,13 +67,13 @@
 	}
 	.modal-content {
 		background-color: #fefefe;
-		position: fixed; /* Fixes the div's position relative to the viewport */
-		top: 50%; /* Position the top edge of the div at the middle of the viewport vertically */
-		left: 50%; /* Position the left edge of the div at the middle of the viewport horizontally */
-		transform: translate(-50%, -50%); /* Offset the div by half its width and height to center it */
+		position: fixed; 
+		top: 50%; 
+		left: 50%; 
+		transform: translate(-50%, -50%); 
 		padding: 20px;
 		border: 1px solid #888;
-		width: 80%; /* Adjust width as necessary */
+		width: 80%;
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 		animation-name: animatetop;
 		animation-duration: 0.4s;
