d42412172694e9486de552d2ab042e2dfe0aff3b
Peter Li <lipet2k@Peters-MacBook-Pro.local>
2024-04-20 13:19:08 -0400

    hardhat

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+    const ballot = await hre.ethers.deployContract("Ballot");
+    await ballot.waitForDeployment();
+
+    console.log(
+        `Ballot deployed to ${ballot.target}`
+    );
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
